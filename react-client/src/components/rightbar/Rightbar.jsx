c29ca77569c12f0d5e9395692d98746a02f9e0f1
Joon Park <joonqkr@berkeley.edu>
2022-04-28 22:15:14 -0700

    start tag filter functionality

--- react-client/src/components/rightbar/Rightbar.jsx
+++ react-client/src/components/rightbar/Rightbar.jsx
@@ -14,12 +14,6 @@ import { IoEarSharp } from "react-icons/io5";
 import { BiDish } from "react-icons/bi";
 
 export default function Rightbar({ user }) {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const [friends, setFriends] = useState([]);
-  const [currentFriends, setCurrentFriends] = useState([]);
-  const [followed, setFollowed] = useState(false);
-  const { user: currentUser, dispatch } = useContext(AuthContext);
-
   const [listOfItems, setListOfItems] = useState([
     {name: 'classes', isChecked: false},
     {name: 'roommates', isChecked: false},
@@ -37,6 +31,8 @@ export default function Rightbar({ user }) {
     {name: 'sad', isChecked: false},
     {name: 'miscellaneous', isChecked: false},
 ]);
+
+const [trueTags, setTrueTags] = useState([]);
 
   const nameToIcon = new Map([
     ['classes', FaBook],
@@ -60,60 +56,18 @@ export default function Rightbar({ user }) {
     const updatedListOfItems = [...listOfItems];
     updatedListOfItems[itemIndex].isChecked = newsChecked;
     setListOfItems(updatedListOfItems);
-};
 
-  useEffect(() => {
-    const getFriends = async () => {
-      try {
-        const friendsList = await axios.get(`/users/friends/${user?._id}`);
-        setFriends(friendsList.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getFriends();
-  }, [user?._id]);
-
-  useEffect(() => {
-    const getCurrentFriends = async () => {
-      try {
-        const currentFriendsList = await axios.get(
-          `/users/friends/${currentUser._id}`
-        );
-        setCurrentFriends(currentFriendsList.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getCurrentFriends();
-  }, [currentUser._id]);
-
-  useEffect(() => {
-    const checkFollowed = async () => {
-      const isFollowed = await currentUser.following.includes(user?._id);
-      setFollowed(isFollowed);
-    };
-    checkFollowed();
-  }, [currentUser, user?._id]);
-
-  const handleClick = async () => {
-    try {
-      if (followed) {
-        await axios.put(`/users/${user._id}/unfollow`, {
-          userId: currentUser._id,
-        });
-        dispatch({ type: "UNFOLLOW", payload: user._id });
-      } else {
-        await axios.put(`/users/${user._id}/follow`, {
-          userId: currentUser._id,
-        });
-        dispatch({ type: "FOLLOW", payload: user._id });
-      }
-    } catch (error) {
-      console.log(error);
+    // Update tags list.
+    if (newsChecked) {
+      trueTags.push(listOfItems[itemIndex].name)
+      setTrueTags(trueTags);
+    } else {
+      trueTags.splice(trueTags.indexOf(listOfItems[itemIndex].name), 1);
+      setTrueTags(trueTags);
     }
-    setFollowed(!followed);
-  };
+
+    // Parent callback function. Set true tags back to Home.
+};
 
   const returnTag = (tagInfo, index) => {
     const Icon = nameToIcon.get(tagInfo.name);
@@ -152,75 +106,14 @@ export default function Rightbar({ user }) {
               </div>
               
         </div>
-        <ul className="rightbarFollowingsHome">
-          {currentFriends.map((u) => (
-            <Online key={u._id} user={u} />
-          ))}
-        </ul>
       </>
     );
   };
 
-  const ProfileRightbar = ({ followed }) => {
-    return (
-      <>
-        {user.username !== currentUser.username && (
-          <button className="rightbarFollowButton" onClick={handleClick}>
-            {followed ? "Unfollow" : "Follow"}
-            {followed ? <Remove /> : <Add />}
-          </button>
-        )}
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City: </span>
-            <span className="rightbarInfoValue">{user.city}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">{user.from}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">
-              {user.relationship === 1
-                ? "Single"
-                : user.relationship === 2
-                ? "Taken"
-                : "-"}
-            </span>
-          </div>
-        </div>
-        <h4 className="rightbarTitle">User friends</h4>
-        <div className="rightbarFollowings">
-          {friends.map((friend) => (
-            <Link
-              key={friend._id}
-              to={`/profile/${friend.username}`}
-              style={{ textDecoration: "none" }}
-            >
-              <div className="rightbarFollowing">
-                <img
-                  src={
-                    friend.profilePicture
-                      ? PF + friend.profilePicture
-                      : PF + "person/noAvatar.png"
-                  }
-                  alt=""
-                  className="rightbarFollowingImg"
-                />
-                <span className="rightbarFollowingName">{friend.username}</span>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </>
-    );
-  };
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-        {user ? <ProfileRightbar followed={followed} /> : <HomeRightbar />}
+        <HomeRightbar />
       </div>
     </div>
   );
--- react-client/src/pages/home/Home.jsx
+++ react-client/src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import Topbar from "../../components/topbar/Topbar";
-import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import { useState } from "react";
 import "./home.css";
 
 export default function Home() {
+  // Array of tags selected.
+  const [tags, setTags] = useState([]);
+
+  // Set parent callback function. (Set tags.)
+  const updateTags = (newTags) => {
+    setTags(newTags);
+  }
+
   return (
     <>
       <Topbar />
